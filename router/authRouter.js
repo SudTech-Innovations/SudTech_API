3099161cecad5380417b399394d640c617c67c3b
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-06-20 16:42:52 +0200

    BCRYPT!!!!!!!!!!!!!!!!

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const verifyBearerToken = require("../middleware/security");
 const codeHandler = require("../util/codeHandler");
+const bcrypt = require("bcrypt");
 
 const User = require("../model/user");
 const userController = require("../controller/userController");
@@ -12,7 +13,8 @@ const expirationTime = 3600;
 router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.create({ username, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
     codeHandler.handle201Success(res, user);
   } catch (error) {
     console.error(error);
@@ -23,23 +25,15 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-
-    if (!user || user.password !== password) {
-      return codeHandler.handle401Error(res);
+    const user = await User.findOne({ where: { username } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ userId: user.id }, process.env.SECRET_TOKEN, {
+        expiresIn: expirationTime,
+      });
+      codeHandler.handle200Success(res, { token, expiresIn: expirationTime });
+    } else {
+      codeHandler.handle401Error(res);
     }
-
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_TOKEN, {
-      expiresIn: expirationTime,
-    });
-
-    const response = {
-      username: user.username,
-      token,
-      expiresIn: expirationTime,
-    };
-
-    codeHandler.handle200Success(res, response);
   } catch (error) {
     console.error(error);
     codeHandler.handle500Error(res);
