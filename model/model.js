d36341dbf9e6860561d3f86bdc8c39abc1bf0339
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-06-24 09:01:09 +0200

    new user when db init

--- model/model.js
+++ model/model.js
@@ -1,9 +1,19 @@
 const sequelize = require("../config/database");
+const dotenv = require("dotenv");
 
 const User = require("./user");
 
 const initializeDatabase = async () => {
   await sequelize.sync({ force: false });
+
+  // Créer un utilisateur par défaut avec les données du fichier .env
+  const users = await User.findAll();
+  if (users.length === 0) {
+    await User.create({
+      username: process.env.DEFAULT_USERNAME,
+      password: process.env.DEFAULT_PASSWORD,
+    });
+  }
 };
 
 module.exports = initializeDatabase;
